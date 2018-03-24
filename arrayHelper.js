ffaa35722156655dc5c2d697512969fafaf8c86f
tonytomk <tonytomk007@gmail.com>
2018-03-24 22:22:51 +0530

    Update arrayHelper.js
    forEach
    map

--- arrayHelper.js
+++ arrayHelper.js
@@ -25,3 +25,19 @@ numbers.forEach(function(num){
 //Print the sum
 sum
 
+var numbers=[1,2,3];
+var doubledNumbers=[];
+
+// conventional approach
+for( var i=0; i<numbers.length; i++)
+{
+doubledNumbers.push(numbers[i] * 2);
+}
+
+// map helper
+
+var doubled=numbers.map(function(number){
+  return number*2;
+});
+doubled                      
+doubledNumbers
