93f0bfe794d220da617a0dec94ee716422464c2f
tonytomk <tonytomk007@gmail.com>
2018-03-25 22:24:55 +0530

    Update arrayHelper.js

--- arrayHelper.js
+++ arrayHelper.js
@@ -41,3 +41,87 @@ var doubled=numbers.map(function(number){
 });
 doubled                      
 doubledNumbers
+
+
+// map example 2
+var cars=[
+  {model:'alto',price:'cheap'},
+  {model:'i20',price:'expensive'}
+];
+
+
+var prices=cars.map(function(car){
+return car.price;
+});
+
+prices
+
+
+//Filter array helper
+
+var products=[
+  {name:'cucumber',type:'vegetable'},
+  {name:'banana',type:'fruit'},
+  {name:'celery',type:'vegetable'},
+  {name:'orange',type:'fruit'}
+];
+
+//conventional approach
+var filtersFruits=[];
+
+for (var i=0;	i<products.length;i++){
+  if(products[i].type === 'fruit'){
+  filtersFruits.push(products[i]);
+  }
+}
+filtersFruits
+
+//Filter array helper
+products.filter(function(product){
+  return product.type==='fruit'
+});
+
+
+//Complex Filter array scenarios
+
+var products=[
+  {name:'cucumber',type:'vegetable',quantity:1,price:10 },
+  {name:'banana',type:'fruit',quantitiy:10,price:40 },
+  {name:'celery',type:'vegetable',quantity:10,price:10 },
+  {name:'orange',type:'fruit',quantity:10,price:40 }
+];
+
+// filter scenario
+//type veg ,price less than 20,qty greater than 5
+
+
+products.filter(function(product){
+return product.type==='vegetable' 
+  && product.quantity>5 
+  && product.price<20
+});
+
+
+
+
+//Complex Filter array scenarios
+
+var posts={id:4,post:'Test post'}
+
+var comments=[
+  {postId:4,content:'kollam'},
+  {postId:3,content:'ok'},
+  {postId:4,content:'awesome'}
+  ];
+
+
+function ComentsForPost(post,comments){
+return comments.filter(function(comment){
+  return comment.postId===4
+});
+}
+
+ComentsForPost(posts,comments)
+
+
+
