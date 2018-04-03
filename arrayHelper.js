d5a71bd342bd51a75ea17b338855806c64ff9b55
tonytomk <tonytomk007@gmail.com>
2018-04-03 12:17:41 +0530

    Update arrayHelper.js

--- arrayHelper.js
+++ arrayHelper.js
@@ -219,3 +219,31 @@ fields.every(function(field){
  return field.validate;
 });
 
+
+
+// Reduce helpers
+
+var numbers=[10,20,30];
+var sum=0;
+
+numbers.reduce(function(sum,number) {
+return sum+number;
+},0);
+
+
+// Reduce helpers Scenario 1
+
+var primaryColors=[
+  {color:"red"},
+  {color:"yellow"},
+  {color:"blue"}  
+];
+
+primaryColors.reduce(function(previous,primaryColor){
+  previous.push(primaryColor.color);
+  return previous;
+  
+}, [] );
+
+
+
