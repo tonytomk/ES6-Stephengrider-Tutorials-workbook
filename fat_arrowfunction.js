9c150adf7051dec9317d838dfd16bc2d038ee98a
tonytomk <tonytomk007@gmail.com>
2018-04-07 22:55:51 +0530

    Update fat_arrowfunction.js

--- fat_arrowfunction.js
+++ fat_arrowfunction.js
@@ -31,3 +31,29 @@ return 2*number;
 ///
 
 numbers.map(number => 2*number);
+
+
+// Fat arrow scenario 1
+
+const team= {
+ members :['Jane','bill'],
+ teamName: 'Super Squad',
+ teamSummary:function(){
+   let self=this;
+   return this.members.map(function(memeber){
+   return  `${memeber} is on team ${self.teamName}`;
+   });
+  //bind(this));
+},
+  //using Fat arrow function
+  //Fat arrow function uses lexical this
+ teamSummary1:function(){
+  	return this.members.map((memeber) => {
+    return  `${memeber} is on team ${this.teamName}`;
+    });
+}
+};
+  
+
+team.teamSummary();
+team.teamSummary1();
