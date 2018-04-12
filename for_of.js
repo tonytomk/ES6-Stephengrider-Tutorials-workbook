635e4bb99628b726b144903d8c5396357be790e5
tonytomk <tonytomk007@gmail.com>
2018-04-12 23:21:03 +0530

    Create for_of.js

---
+++ for_of.js
@@ -0,0 +1,16 @@
+// For Of loop
+const colors=['red','green','blue','yellow']
+
+for(let clr of colors)
+{
+  console.log(clr);
+}
+
+
+const numbers=[1,2,3,4,5]
+
+let total=0;
+
+for(let num of numbers){
+ total+=num;
+}
