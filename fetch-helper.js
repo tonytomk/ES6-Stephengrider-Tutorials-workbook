65af54e1a79d0e7208340d9229cdebc72dfce106
tonytomk <tonytomk007@gmail.com>
2018-04-18 23:19:57 +0530

    Update fetch-helper.js

--- fetch-helper.js
+++ fetch-helper.js
@@ -10,3 +10,12 @@ fetch(url)
     fetch(url)
         .then(response=>response.json())
         .then(data=>console.log(data));
+
+    //whenver the request hits server 
+    // it wont fall to the catch
+    //if ti fails to hit server then only it will go to catch block
+    //disimliar from other ajax request
+    url="https://jsonplaceholder.typicode.com/posts";
+    fetch(url)
+        .then(response=>console.log(response))
+        .catch(error=>console.log('BAD',error));
